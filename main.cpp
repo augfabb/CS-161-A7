e445607cccb1cbe7ec184c54a33d4e823ebe167e
augfabb <augdogwww@gmail.com>
2025-11-16 23:37:22 +0000

    boom

--- main.cpp
+++ main.cpp
@@ -49,17 +49,63 @@ int readInt(string prompt) {
     return input;
 }
 double readScore(string prompt) {
-    cout << prompt;
-    int input = 0;
+    
+    double input = 0;
     bool valid = false;
     while (!valid) {
+        cout << prompt;
         cin >> input;
         if (input >= 0 && input <= 4) {
             valid = true;
         }
+        else {
+            cout << "Invalid input please retry" << endl;
+        }
     }
     return input;
 }
+double assignAverage(int numAssigns){
+    double totalScore = 0.0;
+    double score = 0.0;
+    if (numAssigns == 0) {
+        return 0.0;
+    }
+    else {
+        for (int i = 1; i <= numAssigns; i++) {
+        string scorePrompt = "Enter score " + to_string(i) + ": ";
+        score = readScore(scorePrompt);
+        totalScore += score;
+        }
+        double average = totalScore / numAssigns;
+        return average;
+
+     }
+}
+double calcFinalScore(double assignAvg, double midterm, double final) {
+    double finalScore = (assignAvg * 0.60) + (midterm * 0.20) + (final * 0.20);
+
+    return finalScore;
+}
+char calcLetterGrade(double finalScore) {
+    char letterGrade;
+    if (finalScore >= 3.3) {
+        letterGrade = 'A';
+    }
+    else if (finalScore >=2.8) {
+        letterGrade = 'B';
+    }
+    else if (finalScore >=2.0) {
+        letterGrade = 'C';
+    }
+    else if (finalScore >=1.2) {
+        letterGrade = 'D';
+    }
+    else {
+        letterGrade = 'F';
+    }
+    return letterGrade;
+
+}   
 
 
 
@@ -72,31 +118,43 @@ double readScore(string prompt) {
 
 int main(int argc, char* argv[]) {
     int numAssignments = 0;
+    double averageScore = 0;
+    double midScore = 0;
+    double finalScore = 0;
+    double totalgrade = 0;
     WelcomeMessage();
     //read assignments from user
     numAssignments = readInt("Enter the number of assignments (0 to 10): ");
     
-    //read scores
-    for (int i = 1; i <= numAssignments; i++) {
-        
-    }
-    
-    
-    double score = readScore("enterscore");
-    
+    cout << fixed << setprecision(1);
 
+    //read scores + calc average
+    averageScore = assignAverage(numAssignments);
 
+    //read Midterm and Final score
+    midScore = readScore("Enter your midterm exam score: ");
+    finalScore = readScore("Enter your final exam score: ");
 
+    //output score
+    totalgrade = calcFinalScore(averageScore, midScore, finalScore);
+    cout << "Your Final Numeric score is " << totalgrade << endl;
 
-
-
-
+    char letterGrade = calcLetterGrade(totalgrade);
+    cout << "Your Final Grade is " << letterGrade << endl;
+    cout << "Thanks for using my program!";
 
 
   return 0;
 }
 
-// Function implementations (if any)
+/*Function implementations (if any)
+void WelcomeMessage
+int readInt(string prompt)
+double readScore(string prompt)
+double assignAverage(int numAssigns)
+double calcFinalScore(double assignAvg, double midterm, double final)
+char calcLetterGrade(double finalScore)
+
 
 
 // ------------- DESIGN -------------
@@ -107,24 +165,63 @@ Program Description:
 
 Design:
 A. INPUT
-Define the input variables including name data type. 
+int numAssignments = 0;
+double averageScore = 0;
+double midScore = 0;
+double finalScore = 0;
 
 B. OUTPUT
-Define the output variables including data types. 
+double totalgrade = 0;
+char letterGrade;
+
 
 C. CALCULATIONS
-Describe calculations used by algorithms in step D.  
-List all formulas. 
-If there are no calculations needed, state there are no calculations.
+calcFinalScore(averageScore, midScore, finalScore);
+calcLetterGrade(totalgrade)
+
+
 
 D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+    CALL WelcomeMessage();
+    //read assignments from user
+    numAssignments = CALL readInt("Enter the number of assignments (0 to 10): ");
+    
+    SET PRECISION TO 1
+
+    //read scores + calc average
+    averageScore =  CALL assignAverage(numAssignments);
+
+    //read Midterm and Final score
+    midScore =  CALL readScore("Enter your midterm exam score: ");
+    finalScore =  CALL readScore("Enter your final exam score: ");
+
+    //output score
+    totalgrade = CALL calcFinalScore(averageScore, midScore, finalScore);
+    OUTPUT "Your Final Numeric score is " + totalgrade +endl;
+
+    char letterGrade = calcLetterGrade(totalgrade);
+    OUTPUT "Your Final Grade is " + letterGrade + endl;
+    OUTPUT  "Thanks for using my program!";
+
 
 
 SAMPLE RUNS
-Copy from assignment document.
+Welcome to my final grade calculator!
+Please enter the following information and I will calculate your Final Numerical Grade and Letter Grade for you!
+The number of assignments must be between 0 and 10.
+All scores entered must be between 0 and 4.
+Enter the number of assignments (0 to 10): 
+6
+Enter score 1: 3.4
+Enter score 2: 4
+Enter score 3: 2.5
+Enter score 4: 3.3
+Enter score 5: 3.1
+Enter score 6: 2.5
+Enter your midterm exam score: 3.5
+Enter your final exam score: 4
+Your Final Numeric score is 3.4
+Your Final Grade is A
+Thanks for using my program!
 
 */
